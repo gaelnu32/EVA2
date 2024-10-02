848bf96d7b69e2ee50a95d5483800931d97d89e3
gaelnu32 <L24550335@chihuahua2.tecnm.mx>
2024-10-02 16:53:09 -0600

    Add files via upload

---
+++ EVA2_1_CICLOSS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_cicloss;
+
+/**
+ *
+ * @author lozan
+ */
+public class EVA2_1_CICLOSS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i <= 10; i++) {
+            System.out.println ( i);
+        }
+       System.out.println("");
+       for (int i = 10; i >= 1; i--) {
+            System.out.println ( i);
+        }
+    }
+    
+}
